8b56a32baf9fd08614a37f34623b5d8248d50595
ferdianar <ferdianahmadrozikin10@gmail.com>
2020-06-27 01:41:15 +0700

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     return(
         <div className="header">
             <div className="Greeting">
-                <h1>Hi, I'm Ferdian Ahmad Rozikin. Who is Instructor.</h1>
+                <h1>Hi, I'm Ferdian Ahmad Rozikin. Who is a Frontend Developer.</h1>
                 <p className="description">I am a Frontend Web Developer at Software House located in Malang City. I have skill in Frontend Technologist and create a User Interface Design based User Experience.</p>
                 <div className="action">
                 <button className="calltoaction">Email Me</button>
